f338f757c358aafcb6529cb32a8111afe8db596e
Eduardo Lamhut <elamhut@gmail.com>
2025-12-15 02:39:41 -0300

    working config

--- config.el
+++ config.el
@@ -34,7 +34,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-font (font-spec :family "CaskaydiaCove NFM" :size 20))
+(setq doom-font (font-spec :family "CaskaydiaCove NFM" :size 24))
 (setq doom-symbol-font (font-spec :family "Symbols Nerd Font"))
 
 (load-theme 'kanagawa-wave t)
