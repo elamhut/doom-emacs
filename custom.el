cc5dffd8e6679e1c506bd62ddf9eeaf316906409
Eduardo Lamhut <elamhut@gmail.com>
2025-12-17 16:43:58 -0300

    Multicursor Color is pretty and visible now

--- custom.el
+++ custom.el
@@ -11,4 +11,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(evil-goggles-default-face ((t (:background "orange red"))))
+ '(highlight ((t (:background "#957FB8" :foreground "ghost white" :weight bold))))
  '(region ((t nil))))
