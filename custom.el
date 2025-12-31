b567685425c7a0d02af4b56ed8891f900cc50a9d
Eduardo Lamhut <elamhut@gmail.com>
2025-12-31 09:10:54 -0300

    WIP Cool

--- custom.el
+++ custom.el
@@ -5,7 +5,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("d74f7e7ec49f361008a84715492546270f0f89196b2e96865bbc80edf71ba268"
+   '("e552d09f15c98f84180435e2e9bec7fdd2173c91f5c989592eb92dd148eb9e12"
+     "d74f7e7ec49f361008a84715492546270f0f89196b2e96865bbc80edf71ba268"
      "7c64ffaedc4cf19aeed77e77b7c2d44c5b4911a795e11ec2e9d2aca36e812cdb"
      "8876265830d3656ed77f5e91090ed6e71c0eb6a8e8250f4858bdd5687baf060a"
      "3f997098ca0b9051e879014169f56b5a06348f15334907c60058508d7a22fe3c"
